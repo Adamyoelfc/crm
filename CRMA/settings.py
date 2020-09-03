98f7e426c0ff9e8b5a0aed21ce9cdb86b6a23a9a
Adamyoelfc <adamyoelfc@gmail.com>
2020-09-02 23:18:22 -0400

    heroku

--- CRMA/settings.py
+++ CRMA/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y-^072s7ap_6-)lyy61bljro6==@_wnf$lte87h1n=+y@y9sw5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['adam-crm.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
