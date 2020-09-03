6055c62c7a0e7d46dd0788fd03140ac4ca097a27
Adamyoelfc <adamyoelfc@gmail.com>
2020-09-03 00:21:06 -0400

    setting

--- CRMA/settings.py
+++ CRMA/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y-^072s7ap_6-)lyy61bljro6==@_wnf$lte87h1n=+y@y9sw5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['adam-crm.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['adam-crm.herokuapp.com', 'localhost']
 
 
 # Application definition
